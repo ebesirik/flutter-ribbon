a10d014e180249a4747d236979cbdf5c6156f1ee
Emre Besirik <nomercy@gmail.com>
2021-04-09 10:27:00 +0300

    Null Safety fix

--- lib/ribbon.dart
+++ lib/ribbon.dart
@@ -105,7 +105,7 @@ class _RibbonPainter extends CustomPainter {
 
   void _initializ(Size size) {
     initialized = true;
-    if (nearLength! > farLength!) {
+    if (nearLength > farLength) {
       double temp = farLength;
       farLength = nearLength;
       nearLength = temp;
